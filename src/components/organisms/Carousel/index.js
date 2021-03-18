d835fe25b148857f3a0b6ebd278df1d730bd8ca5
NadezhdaSvetlakova <nadezhda.svetlakova@accenture.com>
2021-03-18 12:33:15 +0200

    fetch and img are working now

--- src/components/organisms/Carousel/index.js
+++ src/components/organisms/Carousel/index.js
@@ -4,12 +4,11 @@ import Button from '../../atoms/Button';
 import './style.scss';
 
 const Carousel = ({title, subtitle, images}) => {
-  console.log(images && images[0].src)
   return (
     <div className="carousel">
       <CarouselText title={title} subtitle={subtitle} />
       <div className="carousel__box middle">
-        <img src={require('../../../images/carousel_img_1.png').default} alt={images && images[0].alt} className="carousel__img" />
+        <img src={images && require(`../../../images/${images[0].src}`).default} alt={images && images[0].alt} className="carousel__img" />
       </div>
       <div className="carousel__buttons">
         <Button className="button-big" arrowDirection="arrow arrow-left arrow-big" />
@@ -20,6 +19,3 @@ const Carousel = ({title, subtitle, images}) => {
 };
 
 export default Carousel;
-
-//images && require(`${images[0].src}`).default
-//require('../../../images/carousel_img_1.png').default
